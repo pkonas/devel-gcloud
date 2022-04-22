b3aa2b0b9df71da546179f40f5ea748c0ff719b5
joravec-svs <joravec@svsfem.cz>
2022-04-22 13:30:03 +0200

    worker

--- dtwin_web/__init__.py
+++ dtwin_web/__init__.py
@@ -3,12 +3,17 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+import rq
+from redis import Redis
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+app.redis = Redis.from_url(app.config['REDIS_URL'])
+app.task_queue = rq.Queue('dtwin-tasks', connection=app.redis)
+rq_job = app.task_queue.enqueue('app.server.start_server')
 
 from dtwin_web import routes,models
 
--- dtwin_web/routes.py
+++ dtwin_web/routes.py
@@ -2,8 +2,6 @@
 from dtwin_web import app
 from flask import render_template, make_response
 import json
-import socket
-import struct
 from dtwin_web import db
 from dtwin_web.models import Data
 
@@ -11,44 +9,13 @@ from dtwin_web.models import Data
 def index():
     return render_template('index.html', title='Home')
 
-def server():
-    hostname, port = "", int(5001)
-    print(f"Starting up on {hostname} port {port}")
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((hostname, port)) 
-    s.listen(1)
-    
-    while True:
-        print("Waiting for connection")
-        clientsocket, address = s.accept()
-        try:
-            print(f"Connection from {address} has been established.")
-            while True:            
-                packet = clientsocket.recv(1024)
-                if packet:
-                    data = struct.unpack('!ddd', packet)
-                    print(f"Data recieved: {data}")
-                    add_data = Data(datetime=data[0],temperature=data[1],humidity=data[2])
-                    db.session.add(add_data)
-                    db.session.commit()
-                else:
-                    print("No more data recieved")
-                    break
-        
-        except KeyboardInterrupt:
-            print("Caught keyboard interrupt, exiting")
-            
-        finally:
-            clientsocket.close()
-            print("Connection closed")
-
 @app.route('/data', methods=["GET", "POST"])            
 def data(): 
     obj = db.session.query(Data.datetime, Data.temperature, Data.humidity).order_by(db.desc('datetime')).first()  
     print(obj)      
     Temperature = obj[1]
     Humidity = obj[2]
-    data = [obj[0] * 1000, Temperature, Humidity]
+    data = [(obj[0] + 7200) * 1000, Temperature, Humidity] #uprava casu aby ukazaoval +2h
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
